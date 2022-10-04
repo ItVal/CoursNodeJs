8140e927343ef0ccc9077abc875d0ee18c5fc8ed
ItVal <valentinhonvj1@gmail.com>
2022-10-04 15:11:23 +0200

    création et suppression dossier via le module fs

--- gestionaireFichier.js
+++ gestionaireFichier.js
@@ -1,8 +1,27 @@
 const fs = require('fs'); //declaration du module fs
 
-//Gestion dossiers
-if(fs.existsSync('./mesFichiers')){   //vérification de l'existance du dossieur
-    console.log ('Dossier existe déjà')
+//Création dossier
+// if(fs.existsSync('./mesFichiers')){   //vérification de l'existance du dossieur
+//     console.log ('Dossier existe déjà')
+// }else{
+//     fs.mkdir('./mesFichiers', (err) =>{ //si le dossieur n'exite pas encore, ici on le crée
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log('Dossier créé avec succes')
+//         }
+//     });
+// }
+
+//Suppression d'un dossier
+if(fs.existsSync('./mesFichiers')){   //là on vérifie si le dossier existe, on le supprime
+    fs.rmdir('./mesFichiers', (err) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Dossier supprimer avec succes')
+        }
+    });
 }else{
     fs.mkdir('./mesFichiers', (err) =>{ //si le dossieur n'exite pas encore, ici on le crée
         if(err){
